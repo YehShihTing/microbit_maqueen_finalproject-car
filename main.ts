ca9604259d294d38939078e94db4b05aabc85fac
YehShihTing <35896516+YehShihTing@users.noreply.github.com>
2021-12-20 23:07:57 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,26 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 150)
+    } else if (receivedNumber == 2) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 150)
+    } else if (receivedNumber == 3) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 150)
+    } else if (receivedNumber == 4) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 150)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+    } else if (receivedNumber == 5) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 150)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 150)
+    } else if (receivedNumber == 6) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 150)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 150)
+    } else if (receivedNumber == 7) {
+        maqueen.motorStop(maqueen.Motors.All)
+    } else if (receivedNumber == 8) {
+        basic.showIcon(IconNames.Yes)
+        basic.pause(500)
+        basic.clearScreen()
+    }
+})
+radio.setGroup(1)
